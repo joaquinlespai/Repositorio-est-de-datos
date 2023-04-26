4f655ea9cec742d1570eef54397a872dc0639a87
joaquinlepasi <joaquinlespaig@gmail.com>
2023-04-25 20:15:24 -0400

    se agregan nuevos archivos

---
+++ apunte2.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+print("ingresando los elemntos de la matris 1 :")
+m1 = np.zeros((fila ,columna))
+for i in range(fila):
+    for j in range(columna):
+        elem = int(input(f"ingrese el elemento [{i}][{j}]"))
+        m1[i][j] = elem
+
+
+print("ingresando los elemntos de la matris 2 :")
+m2 = np.zeros((fila ,columna))
+for i in range(fila):
+    for j in range(columna):
+        elem = int(input(f"ingrese el elemento [{i}][{j}]"))
+        m2[i][j] = elem
+
+m_final_suma = suma(m1,m2)
+print("\nLa matriz resultado de la suma es: ")
+print(m_final_suma)
+
+m_final_resta = resta(m1,m2)
+print("\nLa matriz resultado de la resta es: ")
+print(m_final_resta)
---
+++ apuntes.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def suma(m1,m2):
+    m_final_suma = np.add(m1 + m2)
+    return m_final_suma
+
+def resta(m1,m2):
+    m_final_resta = np.add(m1 - m2) 
+    return m_final_resta
+
+filas = int(input("ingrese la cantidad de filas de la matris: "))
+columnas = int(input("ingrese la cantidad de columnas de la matris: "))
