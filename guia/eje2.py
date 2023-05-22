33c57fe66849f0d769c07aa5e6632d92053b766f
joaquinlepasi <joaquinlespaig@gmail.com>
2023-05-22 01:29:35 -0400

    se agrega nuevo archivo

---
+++ guia/eje2.py
@@ -0,0 +1,18 @@
+import random
+import numpy as np
+
+filas = int(input("ingresa la cantidad de filas que tendra: "))
+columnas = int(input("ingresa la cantiad de columnas que tendra: "))
+
+Ma = [[random.randint(1, 5) for j in range(columnas)] for i in range(filas)]
+
+Mai = np.linalg.inv(Ma)
+
+Mul = np.matmul(Ma, Mai)
+
+Identity = np.eye(filas, columnas)
+
+if np.allclose(Mul, Identity):
+    print("La propiedad A × A^-1 = I se cumple.")
+else:
+    print("La propiedad A × A^-1 = I no se cumple.")
---
+++ guia/eje4.py
@@ -0,0 +1,3 @@
+void main(){
+  List<int> A =  [4,3,2,2,1];
+  print(A);
