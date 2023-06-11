efee9f7dcfb18aeb7048014d23c353547d036349
joaquinlepasi <joaquinlespaig@gmail.com>
2023-06-11 19:26:52 -0400

    se agrega nuevo archivo

--- Estructura de datos/Retoenclases_pilas.py
+++ Estructura de datos/Retoenclases_pilas.py
@@ -1,21 +1,36 @@
-class pilas():
+class stack:
     def __init__(self):
-        self.top = None
-        self.stack = [1,6]
+        self.stack = []
+
     def push(self, item):
-        self.top = item
         self.stack.append(item)
-        return self.top
+
     def pop(self):
         if not self.is_empty():
-            self.stack.pop()
-            self.top = self.stack[-1]
-            return self.top
+            return self.stack.pop()
         else:
-            raise IndexError("el stack es vacio")
-    def __str__(self):
-        return str(self.stack)
+            raise IndexError("el stack esta vacio")
+        
     def is_empty(self):
-        return len(self.stack == 0)
+        return len(self.stack) == 0
+    
     def size(self):
         return len(self.stack)
+    
+pila = stack()
+
+pila.push("libro 1")
+pila.push("libro 2")
+pila.push("libro 3")
+pila.push("libro 4")
+pila.push("libro 5")
+
+print("el estado de la pila despues de apilar los libros: ", pila.stack)
+
+libro = pila.pop()
+print("libro sacado de la pila: ", libro)
+
+libro = pila.pop()
+print("libro sacado de la pila:", libro)
+
+print("estado de la pila despues de sacar libros: ", pila.stack)
