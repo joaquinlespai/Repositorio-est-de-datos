4b27a7119d785de2b47c5783880daa77f9b169fa
joaquinlepasi <joaquinlespaig@gmail.com>
2023-06-11 23:09:06 -0400

    se agrega nuevo archivo

--- Estructura de datos/Retoenclases_colas.py
+++ Estructura de datos/Retoenclases_colas.py
@@ -14,4 +14,37 @@ class cola:
     
     def __str__(self):
         return str(self.items)
-items = int(input("ingresa la cantidad de items que tienes: "))
+
+class Banco:
+    def __init__(self):
+        self.cola_clientes = cola()
+
+    def agregar_cliente(self, nombre_cliente):
+        self.cola_clientes.encolar(nombre_cliente)
+
+    def atender_siguiente_cliente(self):
+        if self.cola_clientes.esta_vacia():
+            print("no hay clientes en espera")
+        else:
+            cliente = self.cola_clientes.desencolar()
+            print("atendinedo cliente:", cliente)
+
+    def mostar_clientes_en_espera(self):
+        print("clientes en espera:", self.cola_clientes)
+
+
+banco = Banco()
+
+
+banco.agregar_cliente("cliente 1")
+banco.agregar_cliente("cliente 2")
+banco.agregar_cliente("cliente 3")
+banco.agregar_cliente("cliente 4")
+banco.agregar_cliente("cliente 5")
+banco.agregar_cliente("cliente 6")
+
+banco.mostar_clientes_en_espera()
+
+banco.atender_siguiente_cliente()
+
+banco.mostar_clientes_en_espera()
