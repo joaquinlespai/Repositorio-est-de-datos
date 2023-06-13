92db89faa99f978aafe3e8aae57b07c2802d2d90
joaquinlepasi <joaquinlespaig@gmail.com>
2023-06-12 23:14:59 -0400

    se agrega nuevo archivo

--- apuntes y ejercicios/Diccionario.py
+++ apuntes y ejercicios/Diccionario.py
@@ -1,8 +1,25 @@
-Paciente1 = {
-    "Nombre" : "",
-    "edad": "",
-    "pesso": "",
-    "Sintomas": ""
-}
-Paciente1["Nombre"] = "pepito"
-print(Paciente1["Nombre"])
+paciente = {}
+
+for i in range(4):
+    nombre = input("ingrese nombre del paciente: ")
+    edad = int(input("ingrese edad del pacinete: "))
+    peso = float(input("ingtrese peso del paciente: "))
+    sintomas = [input("sintomas: "),input(""),input("")]
+
+    paciente[nombre] = {
+        "edad" : edad,
+        "peso" : peso,
+        "sintomas" : sintomas
+    }
+
+
+print("Información del pacinete:")
+for paciente, info in paciente.items():
+    if input("ingrese paciente que busca: ") == paciente: 
+        print(f"paciente: {paciente}")
+        print(f"edad: {info['edad']}")
+        print(f"peso: {info['peso']}")
+        print(f"sintomas: {info['sintomas']}")
+        print()
+    else:
+        print("no se encuentra tal paciente")
